a99b209875446d014d6fd0fce867df69c71de4a8
oncitybb <908057472@qq.com>
2020-08-20 12:00:05 +0800

    Update gupiaorun.sh

--- gupiaorun.sh
+++ gupiaorun.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 findpython = $(ps aux|grep "gp.py" |grep -v "grep"|wc -l)
 if [$findpython -eq 0]
 then
-    nohup /usr/bin/python3 -u /home/ubuntu/gupiao/gp.py > /home/ubuntu/gupiao/tuisong.log 2>&1 &
+    nohup /usr/bin/python3 -u $SHELL_FOLDER/gp.py > $SHELL_FOLDER/tuisong.log 2>&1 &
     echo "开启软件"
 else
     echo "这破软件开着"
